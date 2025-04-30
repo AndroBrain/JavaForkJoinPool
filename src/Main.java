aec01e77643e8279b22b2e312a557d2c2d4810eb
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2025-04-30 11:04:05 +0200

    Cleanup old code

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Queue;
-
-public class Main {
-    public static void main(String[] args) {
-        new Thread(() -> System.out.println("Hello from thread")).start();
-        new Queue<>()
-    }
-}
-
